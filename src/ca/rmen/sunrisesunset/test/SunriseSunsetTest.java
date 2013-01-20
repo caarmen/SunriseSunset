6de8ea98cea72a578ede9eaa25d83b2d8743642d
U-carmenpc\carmen <carmen@carmenpc.(none)>
2013-01-20 23:17:49 +0100

    Adding unit tests for sunrise/sunset

--- src/ca/rmen/sunrisesunset/test/SunriseSunsetTest.java
+++ src/ca/rmen/sunrisesunset/test/SunriseSunsetTest.java
@@ -15,11 +15,15 @@ import ca.rmen.sunrisesunset.SunriseSunset;
 
 public class SunriseSunsetTest {
 
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(
+	private static final SimpleDateFormat DATE_FORMAT_SECONDS = new SimpleDateFormat(
 			"yyyyMMdd HH:mm:ss z");
+	private static final SimpleDateFormat DATE_FORMAT_MINUTES = new SimpleDateFormat(
+			"yyyyMMdd HH:mm z");
+	private static final SimpleDateFormat DATE_FORMAT_DAY = new SimpleDateFormat(
+			"yyyyMMdd z");
 
 	@Test
-	public void test1() {
+	public void testDateConversions1() {
 		test("19760302 15:15:45 UTC", 2442840.1359375);
 		test("19760302 10:15:45 EST", 2442840.1359375);
 		test("19780427 12:00:42 CET", 2443625.9588194);
@@ -41,10 +45,18 @@ public class SunriseSunsetTest {
 	}
 
 	@Test
-	public void test2() {
+	public void testDateConversions2() {
 		test("19000131 00:03:18 UTC", 2415050.502291667);
 		test("18500712 00:17:18 UTC", 2396950.512013889);
+	}
 
+	@Test
+	public void testSunriseSunset() {
+
+		testSunriseSunset("20130120 PST", -118.2437, 34.0522,
+				"20130120 06:57 PST", "20130120 17:11 PST");
+		testSunriseSunset("20130120 CET", 2.351, 48.8567, "20130120 08:35 CET",
+				"20130120 17:28 CET");
 	}
 
 	private void test(String gregorianDateStr, double julianDate) {
@@ -53,26 +65,20 @@ public class SunriseSunsetTest {
 	}
 
 	private void compare(String inputGregorianDateStr, double expectedJulianDate) {
-		try {
-			Date inputGregorianDate = DATE_FORMAT.parse(inputGregorianDateStr);
-			Calendar inputGregorianCal = Calendar.getInstance();
-			inputGregorianCal.setTime(inputGregorianDate);
-			double calculatedJulianDate = SunriseSunset
-					.getJulianDate(inputGregorianCal);
-			double error = expectedJulianDate - calculatedJulianDate;
-			double absError = Math.abs(error);
-			assertTrue("Expected julian date " + expectedJulianDate
-					+ " but got " + calculatedJulianDate
-					+ " for gregorian date " + inputGregorianDateStr,
-					absError < 0.01);
-		} catch (ParseException e) {
-			assertTrue("Error parsing date " + inputGregorianDateStr, false);
-		}
+		Calendar inputGregorianCal = parseDate(DATE_FORMAT_SECONDS,
+				inputGregorianDateStr);
+		double calculatedJulianDate = SunriseSunset
+				.getJulianDate(inputGregorianCal);
+		double error = expectedJulianDate - calculatedJulianDate;
+		double absError = Math.abs(error);
+		assertTrue("Expected julian date " + expectedJulianDate + " but got "
+				+ calculatedJulianDate + " for gregorian date "
+				+ inputGregorianDateStr, absError < 0.01);
 	}
 
 	private void compare(double inputJulianDate, String expectedGregorianDateStr) {
 		try {
-			Date expectedGregorianDate = DATE_FORMAT
+			Date expectedGregorianDate = DATE_FORMAT_SECONDS
 					.parse(expectedGregorianDateStr);
 			Calendar expectedGregorianCal = Calendar.getInstance();
 			expectedGregorianCal.setTime(expectedGregorianDate);
@@ -81,8 +87,8 @@ public class SunriseSunsetTest {
 			// Truncate milliseconds in the calculated result so we can compare
 			// with the expected result.
 			calculatedGregorianCal.set(Calendar.MILLISECOND, 0);
-			String calculatedGregorianDateStr = DATE_FORMAT
-					.format(calculatedGregorianCal.getTime());
+			String calculatedGregorianDateStr = format(DATE_FORMAT_SECONDS,
+					calculatedGregorianCal);
 			Assert.assertEquals("Expected gregorian date "
 					+ expectedGregorianDateStr + " but got "
 					+ calculatedGregorianDateStr + " for Julian date "
@@ -91,6 +97,61 @@ public class SunriseSunsetTest {
 		} catch (ParseException e) {
 			assertTrue("Error parsing date " + expectedGregorianDateStr, false);
 		}
+	}
 
+	private void testSunriseSunset(String inputDayString,
+			double inputLongitude, double inputLatitude,
+			String expectedSunriseString, String expectedSunsetString) {
+		Calendar inputDay = parseDate(DATE_FORMAT_DAY, inputDayString);
+		inputDay.set(Calendar.HOUR_OF_DAY, 12);
+		Calendar expectedSunrise = parseDate(DATE_FORMAT_MINUTES,
+				expectedSunriseString);
+		Calendar expectedSunset = parseDate(DATE_FORMAT_MINUTES,
+				expectedSunsetString);
+		Calendar[] sunriseSunset = SunriseSunset.getSunriseSunset(inputDay,
+				inputLatitude, inputLongitude);
+		// Truncate the seconds. We only care about precision to the minute.
+		Calendar actualSunrise = sunriseSunset[0];
+		Calendar actualSunset = sunriseSunset[1];
+
+		String actualSunriseString = format(DATE_FORMAT_SECONDS, actualSunrise);
+		String actualSunsetString = format(DATE_FORMAT_SECONDS, actualSunset);
+		compare(expectedSunrise, expectedSunriseString, actualSunrise,
+				actualSunriseString, 120000);
+		compare(expectedSunset, expectedSunsetString, actualSunset,
+				actualSunsetString, 120000);
+	}
+
+	private Calendar parseDate(SimpleDateFormat format, String dateString) {
+		try {
+			// Use a clone since parsing may change the timezone
+			SimpleDateFormat formatCopy = (SimpleDateFormat) format.clone();
+			Date date = formatCopy.parse(dateString);
+			Calendar cal = Calendar.getInstance(formatCopy.getTimeZone());
+			cal.setTime(date);
+			return cal;
+		} catch (ParseException e) {
+			Assert.fail("Could not parse date " + dateString);
+			return null;
+		}
+	}
+
+	private String format(SimpleDateFormat format, Calendar calendar) {
+		SimpleDateFormat formatCopy = (SimpleDateFormat) format.clone();
+		formatCopy.setTimeZone(calendar.getTimeZone());
+		return formatCopy.format(calendar.getTime());
+	}
+
+	private void compare(Calendar cal1, String calString1, Calendar cal2,
+			String calString2, long toleranceMillis) {
+		Assert.assertEquals(
+				calString1 + " is in timezone " + cal1.getTimeZone() + " and "
+						+ calString2 + " is in timezone " + cal2.getTimeZone(),
+				cal1.getTimeZone(), cal2.getTimeZone());
+		long timeDifference = cal1.getTimeInMillis() - cal2.getTimeInMillis();
+		long absTimeDifference = Math.abs(timeDifference);
+		Assert.assertTrue(calString1 + " and " + calString2 + " are more than "
+				+ toleranceMillis + " milliseconds apart (" + absTimeDifference
+				+ ")", absTimeDifference < toleranceMillis);
 	}
 }
