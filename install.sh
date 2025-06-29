ff820151e959785ff42ea5306bec08722a89dcf3
Carmen Alvarez <carmen@rmen.ca>
2025-06-29 18:42:02 +0200

    Fix shellcheck issues in install.sh
    Surround variable names with quotes and braces.
    Use $() instead of `` to execute a command and get its output.

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 logprefix=$0
-echo "$logprefix: Rebuilding project..."
+echo "${logprefix}: Rebuilding project..."
 mvn clean package
-echo "$logprefix: Project rebuilt".
-echo "$logprefix: Extracting version from pom file..."
-version=`mvn -q exec:exec -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive`
-echo "$logprefix: Version is $version."
-echo "$logprefix: Installing to local maven repository..."
+echo "${logprefix}: Project rebuilt".
+echo "${logprefix}: Extracting version from pom file..."
+version=$(mvn -q exec:exec -Dexec.executable="echo" -Dexec.args="\${project.version}" --non-recursive)
+echo "${logprefix}: Version is $version."
+echo "${logprefix}: Installing to local maven repository..."
 mvn install -f publish.pom
-mvn install:install-file -Dfile=library/target/lib-sunrise-sunset-$version.jar -Djavadoc=library/target/lib-sunrise-sunset-$version-javadoc.jar -Dsources=library/target/lib-sunrise-sunset-$version-sources.jar
-echo "$logprefix: Done."
+mvn install:install-file \
+  -Dfile="library/target/lib-sunrise-sunset-$version.jar" \
+  -Djavadoc="library/target/lib-sunrise-sunset-${version}-javadoc.jar" \
+  -Dsources="library/target/lib-sunrise-sunset-${version}-sources.jar"
+echo "${logprefix}: Done."
