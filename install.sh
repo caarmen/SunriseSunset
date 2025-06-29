865fda0a13f833a86d88e3d996be70fd19f9e929
Carmen Alvarez <carmen@rmen.ca>
2025-06-29 18:43:21 +0200

    install.sh: If the GPGKEY environment variable is set, sign the arifacts.

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 logprefix=$0
 echo "${logprefix}: Rebuilding project..."
-mvn clean package
+mvn clean
+if [ "${GPGKEY}" != "" ]
+then
+  # To sign the package, use the following command to find the name of your gpg key:
+  # gpg --list-signatures --keyid-format 0xshort
+  # The name of the key is the first token after "sig 3".
+  # See https://central.sonatype.org/publish/requirements/gpg/#listing-keys for more info.
+  # Set the environment variable GPGKEY to the name of the key to use.
+  mvn -Dgpg.keyname="${GPGKEY}" package gpg:sign
+else
+  mvn package
+fi
 echo "${logprefix}: Project rebuilt".
 echo "${logprefix}: Extracting version from pom file..."
 version=$(mvn -q exec:exec -Dexec.executable="echo" -Dexec.args="\${project.version}" --non-recursive)
